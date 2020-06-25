d6bd6d4681686456926164ceb0c9e7748e66a3b5
Tian Zhou <yanxiangtianji@gmail.com>
2020-06-25 16:30:24 -0400

    isolate dying message

--- Code/nomad/msg_type.h
+++ Code/nomad/msg_type.h
@@ -3,8 +3,9 @@
 
 enum MsgType : int8_t{
 	DATA = 0,
-	TERMINATION = 1,
-	LOCAL_ERROR = 2,
+	LOCAL_ERROR = 1,
+	LOCAL_DYING = 2,
+	TERMINATION = 5,
 	CP_START = 11,
 	CP_CLEAR = 12,
 	CP_LFINISH = 13,
